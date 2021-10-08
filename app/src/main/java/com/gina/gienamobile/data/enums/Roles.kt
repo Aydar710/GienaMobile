74ffb022fcb38ab242deae11a2f714e4edfc12f2
vladalekseev <valekseev@dataarmor.ru>
2021-10-09 00:59:37 +0300

    :sparkles: fix user model

---
+++ app/src/main/java/com/gina/gienamobile/data/enums/Roles.kt
@@ -0,0 +1,5 @@
+package com.gina.gienamobile.data.enums
+
+enum class Roles {
+    STUDENT, AVERAGE, OLD
+}
--- app/src/main/java/com/gina/gienamobile/data/model/User.kt
+++ app/src/main/java/com/gina/gienamobile/data/model/User.kt
@@ -1,11 +1,12 @@
 package com.gina.gienamobile.data.model
+import com.gina.gienamobile.data.enums.Roles
 import com.google.gson.annotations.SerializedName
 
 data class User(
     @SerializedName("ID")
     val id: String,
-    @SerializedName("Name")
-    val name: String,
-    @SerializedName("Old")
-    val old: Number
+    @SerializedName("Username")
+    val username: String,
+    @SerializedName("Role")
+    val role: Roles
 )
