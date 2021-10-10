b89fcf073631978dd0ad22f382852ac10e655483
AydarS <asabirzyanov@vtb.ru>
2021-10-10 10:26:00 +0300

    fix greeting

--- app/src/main/java/com/gina/gienamobile/presentation/greeting/GreetingActivity.kt
+++ app/src/main/java/com/gina/gienamobile/presentation/greeting/GreetingActivity.kt
@@ -1,18 +1,19 @@
 package com.gina.gienamobile.presentation.greeting
 
+import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.gina.gienamobile.R
+import com.gina.gienamobile.presentation.entername.EnterNameActivity
 
 class GreetingActivity : AppCompatActivity(R.layout.activity_greeting) {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
         findViewById<ConstraintLayout>(R.id.clRoot).setOnClickListener {
-
+            startActivity(Intent(this, EnterNameActivity::class.java))
         }
     }
 }
